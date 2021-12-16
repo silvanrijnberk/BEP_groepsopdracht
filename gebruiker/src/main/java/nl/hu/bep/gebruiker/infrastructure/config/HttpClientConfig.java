03724b8abd6cca53bf8c0a2360a02e19cb9b504e
silvanrijnberk <silvanrijnberk@live.com>
2021-12-16 14:25:38 +0100

    push

--- gebruiker/src/main/java/nl/hu/bep/gebruiker/infrastructure/config/HttpClientConfig.java
+++ gebruiker/src/main/java/nl/hu/bep/gebruiker/infrastructure/config/HttpClientConfig.java
@@ -1,6 +1,5 @@
 package nl.hu.bep.gebruiker.infrastructure.config;
 
-import nl.hu.bep.gebruiker.infrastructure.driven.storage.HttpAdresRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +15,4 @@ public class HttpClientConfig {
         return new RestTemplate();
     }
 
-    @Bean
-    public HttpAdresRepository httpAdresRepository(){
-        return new HttpAdresRepository(rootPath, restTemplate());
-    }
 }
--- gebruiker/src/main/java/nl/hu/bep/gebruiker/infrastructure/driven/storage/AdresResult.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.bep.gebruiker.infrastructure.driven.storage;
-
-import java.util.UUID;
-
-public class AdresResult {
-    public UUID id;
-}
